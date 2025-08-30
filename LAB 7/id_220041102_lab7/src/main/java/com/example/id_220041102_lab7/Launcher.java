8673210f8634d93c2ac5f090011f2dee6ce6407f
Rafat Abdullah Pantho <rafatpantho@gmail.com>
2025-08-30 20:32:04 +0600

    lab 7 done

--- LAB 7/id_220041102_lab7/src/main/java/com/example/id_220041102_lab7/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.id_220041102_lab7;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
